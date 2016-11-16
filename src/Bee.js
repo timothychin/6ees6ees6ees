941307549f462b62c97a240a950bec84f17beb5e
Tim Chin <timchin@Tims-MacBook-Pro.local>
2016-11-15 20:59:38 -0800

    Passes all tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,7 +1,12 @@
 import Grub from './Grub'
 
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor(age, color, job) {
+    super()
+  	this.age = 5;
+  	this.color = 'yellow';
+  	this.job = 'Keep on growing';
+  }
 };
 
 export default Bee;
--- src/Grub.js
+++ src/Grub.js
@@ -1,4 +1,12 @@
 class Grub {
+  constructor(age, color, food) {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly'
+  }
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
 };
 
 export default Grub;
